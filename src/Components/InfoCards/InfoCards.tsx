19d56a88c77af0632494308d10a7a5f6726cd9f3
salahmander <salahabdo@hotmail.co.uk>
2024-09-02 17:38:36 +0100

    feat: clean up, add text, and setup localization for portfolio section

--- src/Components/InfoCards/InfoCards.tsx
+++ src/Components/InfoCards/InfoCards.tsx
@@ -1,25 +1,31 @@
-export const InfoCards = ({ data }) => {
+type InfoCardTypes = {
+  id: number;
+  img: string;
+  title: string;
+  description: string;
+};
+
+export const InfoCards = ({ data }: { data: InfoCardTypes[] }) => {
   return (
     <>
-      {data.map(({ img, title, text }) => (
-        <div className=" group bg-bg_secondary rounded-lg p-5">
+      {data.map(({ img, title, description, id }) => (
+        <div
+          key={id}
+          className="group cursor-pointer bg-bg_secondary rounded-lg p-5 relative overflow-hidden rounded-xl p-6 bg-bg_secondary z-[1] after:absolute after:top-1/2 after:left-1/2 after:w-full after:h-full after:rounded-full after:-translate-y-1/2 after:-translate-x-1/2 after:bg-gradient-to-b after:from-black_500 after:to-transparent after:z-[-1] after:scale-0 hover:after:scale-100 hover:after:rounded-none after:transition-all after:duration-300 after:ease-in-out hover:shadow-lg"
+        >
           <div className="img relative">
             <div className="overflow-hidden aspect-[3/2] bg-bg_tertiary">
-              <img
-                src={img}
-                alt={title}
-                className="group-hover:scale-125 duration-300 transition-transform"
-              />
+              <img src={img} alt={title} />
             </div>
           </div>
           <div className="content py-4 border-b-2 border-b-black_100">
             <h3
-              className="text-xl font-semibold line-clamp-1 text-heading_color"
+              className="text-center text-xl font-semibold line-clamp-1 text-heading_color"
               title={title}
             >
               {title}
             </h3>
-            <p className="line-clamp-4 text-sm mt-3">{text}</p>
+            <p className="line-clamp-8 text-sm mt-3">{description}</p>
           </div>
         </div>
       ))}
--- src/Sections/Portfolio/Portfolio.tsx
+++ src/Sections/Portfolio/Portfolio.tsx
@@ -1,30 +1,59 @@
+import { MDText } from "i18n-react";
+
 import { SectionHeader } from "../../Components/SectionHeader/SectionHeader";
 import { InfoCards } from "../../Components/InfoCards/InfoCards";
+
+import LocalTexts from "./Portfolio.json";
+
+const LocalT = new MDText(LocalTexts);
 
 const portfolio = [
   {
     id: 1,
-    title: "How to Inject Humor & Comedy Into Your Brand",
-    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odit rerum quos ipsa repellat? Adipisci cumque consequuntur praesentium harum similique, modi esse facere, quis aperiam rem molestias quia? Porro, nesciunt cum! Impedit quidem a eaque possimus, quae magnam suscipit explicabo?`,
-    img: "/assets/portfolio_1/portfolio-1.png",
+    img: "https://picsum.photos/300/200",
+    title: LocalT.translate("portfolio.personal.title") as string,
+    description: LocalT.translate("portfolio.personal.description") as string
   },
   {
     id: 2,
-    title: "Mastering the Art of Programming.",
-    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odit rerum quos ipsa repellat? Adipisci cumque consequuntur praesentium harum similique, modi esse facere, quis aperiam rem molestias quia? Porro, nesciunt cum! Impedit quidem a eaque possimus, quae magnam suscipit explicabo?`,
-    img: "/assets/portfolio_1/portfolio-2.png",
+    img: "https://picsum.photos/300/200",
+    title: LocalT.translate("portfolio.challenges.title") as string,
+    description: LocalT.translate("portfolio.challenges.description") as string
   },
   {
     id: 3,
-    title: "Crafting the Modern Web Experience.",
-    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odit rerum quos ipsa repellat? Adipisci cumque consequuntur praesentium harum similique, modi esse facere, quis aperiam rem molestias quia? Porro, nesciunt cum! Impedit quidem a eaque possimus, quae magnam suscipit explicabo?`,
-    img: "/assets/portfolio_1/portfolio-3.png",
+    img: "https://picsum.photos/300/200",
+    title: LocalT.translate("portfolio.commissioned.title") as string,
+    description: LocalT.translate("portfolio.commissioned.description") as string
   },
   {
     id: 4,
-    title: "How to Own Your Audience by Creating an Email List.",
-    text: `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium odit rerum quos ipsa repellat? Adipisci cumque consequuntur praesentium harum similique, modi esse facere, quis aperiam rem molestias quia? Porro, nesciunt cum! Impedit quidem a eaque possimus, quae magnam suscipit explicabo?`,
-    img: "/assets/portfolio_1/portfolio-4.png",
+    img: "https://picsum.photos/300/200",
+    title: LocalT.translate("portfolio.clones.title") as string,
+    description: LocalT.translate("portfolio.clones.description") as string
+  },
+];
+
+const ForEachPage = [
+  {
+    title: "Personal Projects",
+    description:
+      "Innovative Solutions, My Way: A collection of projects driven by passion and curiosity. Each one showcases unique problem-solving and creativity, highlighting my technical growth and personal interests.",
+  },
+  {
+    title: "Challenges & Experiments",
+    description:
+      "Exploration, Learning, and Problem-Solving: A dynamic mix of projects where I push boundaries, experiment with new technologies, and tackle coding puzzles. This section reflects my journey of continuous learning and creative exploration in various aspects of development.",
+  },
+  {
+    title: "Commissioned Work",
+    description:
+      "Professional Results, Real Impact: A showcase of tasks and projects completed for clients and businesses. This section highlights my ability to deliver high-quality work on time, tailored to meet specific needs and objectives.",
+  },
+  {
+    title: "Website Clones",
+    description:
+      "Reimagining the Web: Replicas of well-known websites, recreated to refine my skills and understand industry standards. These projects demonstrate my attention to detail and ability to replicate complex designs and functionalities.",
   },
 ];
 
@@ -32,7 +61,7 @@ export const Portfolio = () => {
   return (
     <section className={`container pt-section`} id="portfolio">
       <SectionHeader shadowText="Portfolio" title="My Latest Portfolio" />
-      <div className="portfolio mt-10 grid gap-4 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
+      <div className="portfolio m-10 grid gap-4 grid-cols-1 sm:grid-cols-3 lg:grid-cols-4">
         <InfoCards data={portfolio} />
       </div>
     </section>

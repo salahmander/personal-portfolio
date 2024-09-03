2c1a3bf2ea2f58abb755d716ce4c26b6a1938a05
salahmander <salahabdo@hotmail.co.uk>
2024-09-03 12:39:27 +0100

    feat: make the mobile navigation functional

--- src/Components/Header/MobileNavigation.tsx
+++ src/Components/Header/MobileNavigation.tsx
@@ -8,34 +8,48 @@ type mobileNavProps = {
 
 export const MobileNav = ({ toggle, handleToggle }: mobileNavProps) => {
   return (
-    <div className="mobileNav fixed top-0 -right-full h-screen w-full max-w-[300px] overflow-y-auto bg-bg_secondary z-50 overflow-x-hidden md:hidden">
-      <div className="px-paddingX py-3">
-        <div className="flex justify-between items-center">
-          <button
-            className="menu text-3xl py-2 md:hidden"
-            type="button"
-            title="menu"
-            onClick={handleToggle}
-          >
-            {toggle ? <CrossIcon /> : <MenuIcon />}
-          </button>
+    <>
+      <div
+        className={`fixed inset-0 bg-black transition-opacity duration-500 ease-in-out z-40 ${
+          toggle
+            ? "opacity-50 overflow-hidden"
+            : "opacity-0 pointer-events-none"
+        }`}
+        onClick={handleToggle} // Close the menu when the overlay is clicked
+      />
+      <div
+        className={`fixed top-0 h-screen w-full max-w-[300px] overflow-y-auto bg-bg_secondary z-50 transition-transform duration-500 ease-in-out transform ${
+          toggle ? "translate-x-0" : "-translate-x-full"
+        } md:hidden`}
+      >
+        <div className="px-paddingX py-3">
+          <div className="flex justify-between items-center">
+            <button
+              className="menu text-3xl py-2 md:hidden"
+              type="button"
+              title="menu"
+              onClick={handleToggle}
+            >
+              {toggle ? <CrossIcon /> : <MenuIcon />}
+            </button>
+          </div>
+          <nav className="mt-6">
+            <ul>
+              {navLinks.map((link) => (
+                <li key={link.url} onClick={handleToggle}>
+                  <button
+                    type="button"
+                    className={`${link.title}-link transition-all duration-500 py-3 hover:pl-2.5 hover:text-primary_color`}
+                    onClick={() => console.log("click")}
+                  >
+                    {link.title}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </div>
-        <nav className="mt-6">
-          <ul>
-            {navLinks.map((link) => (
-              <li key={link.url} onClick={handleToggle}>
-                <button
-                  type="button"
-                  className={`${link.title}-link transition-all duration-500 py-3 hover:pl-2.5 hover:text-primary_color`}
-                  onClick={() => console.log("click")}
-                >
-                  {link.title}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </nav>
       </div>
-    </div>
+    </>
   );
 };
